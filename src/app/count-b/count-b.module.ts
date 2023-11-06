51ce0517fb4dac176592fe93da1d8467f678786a
Scott Meadows <scottmeadows04@gmail.com>
2023-11-06 15:03:36 -0500

    Different modules demo

--- src/app/count-b/count-b.module.ts
+++ src/app/count-b/count-b.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CountDisplayAComponent } from './count-display-a/count-display-a.component';
+import { CountDisplayBComponent } from './count-display-b/count-display-b.component';
 import { CountService } from '../services/count.service';
 
+
 @NgModule({
-  declarations: [CountDisplayAComponent],
+  declarations: [CountDisplayBComponent],
   imports: [CommonModule],
-  exports: [CountDisplayAComponent],
+  exports: [CountDisplayBComponent],
   providers: [CountService],
 })
-export class CountAModule {}
+export class CountBModule {}
